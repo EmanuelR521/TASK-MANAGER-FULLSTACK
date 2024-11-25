69f032b4bb65f2cb8369ecf9e3bb7270b4cae9b8
Emanuel Rios <118300687+EmanuelR521@users.noreply.github.com>
2024-11-24 20:07:06 -0500

    feat: add password encrypted

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,17 +1,28 @@
 import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs';
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
 
   try {
+
+    const passwordHash = await bcrypt.hash(password, 10)
+
     const newUser = new User({
       email,
-      password,
+      password: passwordHash,
       username,
     });
 
-    await newUser.save();
-    res.send("registrando");
+    const userSaved = await newUser.save();
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+
+    });
   } catch (error) {
     console.log(error);
   }
