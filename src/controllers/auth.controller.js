d968ca5143873f4e45f2aca1224946cd3598bc1d
Emanuel Rios <118300687+EmanuelR521@users.noreply.github.com>
2024-11-26 03:39:04 -0500

    feat: add logout route and controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -62,3 +62,10 @@ export const login = async (req, res) => {
 
   
 };
+
+export const logout = async (req, res) => {
+  res.cookie('token',"", {
+    expires: new Date(0),
+  });
+  res.sendStatus(200);
+}
