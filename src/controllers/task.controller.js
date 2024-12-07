b4a75a65c8734df165e57a5e9e1526818b973a27
EmanuelR521 <emanuel.riosb@upb.edu.co>
2024-12-06 19:54:38 -0500

    feat: add deleteTask & getTasks controllers

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -13,11 +13,25 @@ export const createTask = async (req,res) => {
         date
     })
 
-    await newTask.save();
+    const savedTask = await newTask.save();
+    res.json(savedTask);
 };
 
-export const getTask = async (req,res) => {};
+export const getTask = async (req,res) => {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+        return res.status(404).json({message: "Task not found"});
+    }
+    res.json(task);
+};
 
 export const updateTask = async (req,res) => {};
 
-export const deleteTask = async (req,res) => {};
+export const deleteTask = async (req,res) => {
+
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+        return res.status(404).json({message: "Task not found"});
+    }
+    res.json(task);
+};
