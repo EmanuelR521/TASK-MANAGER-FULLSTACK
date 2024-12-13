49bf228c7126c2ba858357e53872a989f366e3c2
EmanuelR521 <emanuel.riosb@upb.edu.co>
2024-12-13 02:27:26 -0500

    feat: add login validation Schema

--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -17,3 +17,16 @@ export const registerSchema = z.object({
         message: 'Password must be at least 6 characters long'
     })
 });
+
+export const loginSchema = z.object({ 
+    email: z.string({
+        required_error: 'Email is required'
+    }).email({
+        message: 'Invalid email address'
+    }),
+    password: z.string({
+        required_error: 'Password is required'
+    }).min(6, {
+        message: 'Password must be at least 6 characters long'
+    })
+});
